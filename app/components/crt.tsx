d959d1ca467407eeda368a0b20ddb7cca3d8d871
love skön <loveskon335@gmail.com>
2026-09-19 12:46:11 +0200

    Nvm name too long

--- app/components/crt.tsx
+++ app/components/crt.tsx
@@ -106,7 +106,7 @@ const PROJECTS: Record<string, string[]> = {
   'SAAF': [
     'LOADING SAAF.APP...',
     '',
-    'Building web-tech for Svensk AmatörAstronomisk Förening.',
+    'Building web-tech for SAAF.',
     'Supports a remote access telescope.',
     '',
     'STACK: PHP, JavaScript, Python, Wordpress',
